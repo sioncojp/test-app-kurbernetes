858efc7bfc59711dba24c3739f20f50397ff5b12
sioncojp <sion_cojp@yahoo.co.jp>
2018-03-16 13:41:46 +0900

    add healthcheck

--- main.go
+++ main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello World")
-	},
-	)
+	})
+	http.HandleFunc("/healthcheck", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
 	http.ListenAndServe(":8000", nil)
 }

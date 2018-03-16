9b9b8aae0df60fd1319bfc63b6dcf8108c0ad1a9
sioncojp <sion_cojp@yahoo.co.jp>
2018-03-16 14:20:11 +0900

    add graceful shutdown

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"sync/atomic"
 	"time"
 )
 
@@ -13,6 +15,10 @@ type key int
 
 const (
 	requestIDKey key = 0
+)
+
+var (
+	healthy int32
 )
 
 func main() {
@@ -28,22 +34,52 @@ func main() {
 	}
 
 	server := &http.Server{
-		Addr:     ":8000",
-		Handler:  tracing(nextRequestID)(logging(logger)(router)),
-		ErrorLog: logger,
+		Addr:         ":8000",
+		Handler:      tracing(nextRequestID)(logging(logger)(router)),
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
 	}
 
+	done := make(chan bool)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	go func() {
+		<-quit
+		logger.Println("Server is shutting down...")
+		atomic.StoreInt32(&healthy, 0)
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		server.SetKeepAlivesEnabled(false)
+		if err := server.Shutdown(ctx); err != nil {
+			logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+		}
+		close(done)
+	}()
+
 	logger.Println("Server is ready to handle requests at 8000")
+	atomic.StoreInt32(&healthy, 1)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 		logger.Fatalf("Could not listen on 8000 : %v\n", err)
 	}
+
+	<-done
+	logger.Println("Server stopped")
 }
 
 func index(w http.ResponseWriter, req *http.Request) {
-	w.Write([]byte("Hello world"))
+	w.Write([]byte("Hello worldssss"))
 }
 
 func healthcheck(w http.ResponseWriter, req *http.Request) {
+	if atomic.LoadInt32(&healthy) == 1 {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
 }
 
